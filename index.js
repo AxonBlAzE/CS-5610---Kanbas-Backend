383a95b7ad07966c80117ad47c561b434228884a
shreyans <shreyansms@gmail.com>
2024-11-10 10:18:01 -0500

    completed lab work

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from "cors";
 import Hello from "./Hello.js"
 import Lab5 from './Lab5/index.js';
 
+
 const app = express()
+app.use(cors());
+app.use(express.json());
 
 Hello(app)
 Lab5(app)
